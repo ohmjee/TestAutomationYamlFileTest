fdbd9f394c0e92fa64ff8df0521fcc1d095f1018
OHM\ohmje <ohmaditya1526@gmail.com>
2022-12-12 19:15:01 +0530

    Added Logger Info messages and Asset Statement added

--- src/test/java/UIAutomation/Page/OrangeHRMLogin.java
+++ src/test/java/UIAutomation/Page/OrangeHRMLogin.java
@@ -56,7 +56,7 @@ public class OrangeHRMLogin {
         String password = RVFCYF.propFile("Password");
         logger.info("Username:"+username);
         logger.info("Password:"+password);
-        Assert.assertEquals(username,"Admin");
+        Assert.assertEquals("Admin",username);
         driver.findElement(By.xpath("//input[@name='username']")).sendKeys(username);
         driver.findElement(By.xpath("//input[@name='password']")).sendKeys(password);
         driver.findElement(By.xpath("//button[@type='submit']")).click();
